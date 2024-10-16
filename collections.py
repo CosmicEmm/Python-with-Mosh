b73f9a34e17e038bdc80ae241d4aa5d1efaec77a
CosmicEmm <muhammadmalik5303@gmail.com>
2024-10-16 19:23:55 +0500

    Update collections.py

--- collections.py
+++ collections.py
@@ -15,3 +15,32 @@ straw_hats = ("Luffy", "Zoro", "Sanji", "Nami", "Robin")
 red, green, yellow, orange, purple = straw_hats
 print(red)
 print(f"{green} has a crush on {purple}.")
+
+#Dictionary: used to store information in the form of key-value pairs
+customer = {
+    "name" : "Mohammad",
+    "age" : 27,
+    "is_verified" : True
+}
+#we can access an item in a dictionary using its key.
+print(customer["age"])
+#to avoid displaying an error on the terminal when we enter the wrong key, we can use the get method.
+print(customer.get("height"))
+#we can optionally supply a default value for the key that isn't present in the dictionary.
+print(customer.get("mood", "happy"))
+#we can update the value associated with a key in the dictionary.
+customer["name"] = "Muhammad"
+print(customer["name"])
+#we can add a new key-value pair to the dictionary.
+customer["birth_year"] = 1996
+print(customer)
+
+#EXERCISE: 
+numbers = {
+    "1" : "One",
+    "2" : "Two",
+    "3" : "Three",
+    "4" : "Four"
+}
+phone = input("Phone: ")
+print(numbers[phone[0]] + " " + numbers[phone[1]] + " " + numbers[phone[2]] + " " + numbers[phone[3]])
