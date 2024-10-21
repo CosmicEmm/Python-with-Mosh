2be32d9c1241b73f367f3994890bc0e7c2656b17
CosmicEmm <muhammadmalik5303@gmail.com>
2024-10-21 16:59:22 +0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -12,6 +12,33 @@ greetings(last_name="Einstein", first_name="Albert")
 #when mixing both positional and keyword arguments, always use the positional arguments before the keyword arguments
 greetings("Beth", last_name="Harmon")
 print("Finish")
+#Return Statement
+def square(number):
+    return number ** 3
 
 
+result = square(2)
+print(result)
+
+def addition(first_number, second_number):
+    print(first_number + second_number)
+
+
+print(addition(6, 4)) #When we don't have a return statement in the function, Python returns None by default.
+
+#Exercise: Reorganize the Emoji Converter code into a function
+def emoji_converter(message):
+    words = message.split(" ")
+    emojis = {
+        ":)" : "😊",
+        ":(" : "😔"
+    }
+    output = ""
+    for word in words:
+        output += emojis.get(word, word) + " "
+    return output
+
+
+message = input(">")
+print(emoji_converter(message))
 
