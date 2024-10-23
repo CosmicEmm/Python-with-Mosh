6362076bcca52b93a3b731e25de7cc23b2c877eb
CosmicEmm <muhammadmalik5303@gmail.com>
2024-10-23 23:34:23 +0500

    Update scratch pad.py

--- scratch pad.py
+++ scratch pad.py
@@ -1,4 +1,10 @@
-x = 3>2
-print(x)
-x = 3==2
-print(x)
+temperature = 25
+if temperature > 30:
+    print("It's a hot day.")
+    print("Drink plenty of water.")
+elif temperature > 20:
+    print("It's a nice day.")
+elif temperature > 10:
+    print("It's a bit cold.")
+else: #this block of code will get executed if none of the above conditions are true
+    print("It's cold.")
