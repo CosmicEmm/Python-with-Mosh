3472270c70ac6ba21cc59036a3552ff70bd6f403
CosmicEmm <muhammadmalik5303@gmail.com>
2024-10-23 23:53:18 +0500

    Update scratch pad.py

--- scratch pad.py
+++ scratch pad.py
@@ -1,10 +1,6 @@
-temperature = 25
-if temperature > 30:
-    print("It's a hot day.")
-    print("Drink plenty of water.")
-elif temperature > 20:
-    print("It's a nice day.")
-elif temperature > 10:
-    print("It's a bit cold.")
-else: #this block of code will get executed if none of the above conditions are true
-    print("It's cold.")
+weight = float(input("Weight: "))
+unit = input("(K)g or (L)bs: ")
+if unit.upper() == "K":
+    print("Weight in lbs: " + str(weight / 0.45))
+else:
+    print("Weight in kg: " + str(weight * 0.45))
