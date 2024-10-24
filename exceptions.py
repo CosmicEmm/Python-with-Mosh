66a4804c43cff5b373772345cef4c799a41134c1
CosmicEmm <muhammadmalik5303@gmail.com>
2024-10-24 18:09:15 +0500

    Update

---
+++ exceptions.py
@@ -0,0 +1,27 @@
+#Exceptions: errors detected during execution are called exceptions.
+age = int(input("Age: "))
+print(age)
+
+#Terminal Terminology:
+#exit code 0: program completed successfully
+#exit code 1: program crashed
+
+#ValueError: this error occurs when we try to convert a non-numerical value into an integer
+#In Python, we have a construct called try-except to handle errors
+try:
+    age = int(input("Age: "))
+    print(age)
+except ValueError: #this block of code will be executed if an error of the type ValueError is detected
+    print("Invalid value")
+
+try:
+    age = int(input("Age: "))
+    income = 20000
+    risk = income/age
+    print(age)
+except ValueError:
+    print("Invalid value")
+except ZeroDivisionError: #we cannot divide a number by 0
+    print("Age cannot be zero")
+
+
