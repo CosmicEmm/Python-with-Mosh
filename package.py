5a283b4db9490bbe0d7fe90fce42f47935c116d5
CosmicEmm <muhammadmalik5303@gmail.com>
2024-10-29 14:40:23 +0500

    Update

--- package.py
+++ package.py
@@ -10,3 +10,29 @@ calc_shipping()
 from ecommerce import shipping
 shipping.calc_shipping()
 
+#random is one of Python's in-built modules
+import random
+
+#return a random value between 0 and 1
+print(random.random())
+
+#return a random value from a particular range
+for i in range(3):
+    print(random.randint(10,20))
+
+#randomnly return an item from a list
+members = ["Luffy", "Zoro", "Sanji", "Nami", "Robin", "Chopper"]
+captain = random.choice(members)
+print(captain)
+
+#EXERCISE: Write a program to roll a dice. Every time we call the roll method, we get a tuple of two random values.
+class Dice:
+    def roll(self):
+        first = random.randint(1,6)
+        second = random.randint(1,6)
+        return first, second
+
+
+dice = Dice()
+print(dice.roll())
+
