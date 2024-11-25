9309632a53ca4dd92b58cd425c875ade0778419d
CosmicEmm <muhammadmalik5303@gmail.com>
2024-11-25 20:37:22 +0500

    Updated

---
+++ scratch_pad.py
@@ -0,0 +1,12 @@
+def name():
+    boy = 'ali'
+    print(boy)
+    def gender():
+        sex = 'male'
+        def age():
+            a = 24
+            print(f'{boy} is naughty')
+        age()
+    gender()
+
+name()
