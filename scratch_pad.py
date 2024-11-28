139a995f7401008a96ce7335cbe821e7fe4ac3c7
CosmicEmm <muhammadmalik5303@gmail.com>
2024-11-28 16:36:14 +0500

    Update scratch_pad.py

--- scratch_pad.py
+++ scratch_pad.py
@@ -1,15 +1,2 @@
-def multiply_numbers(a=5, b=6):
-    """
-    Multiplies two numbers and returns the result.
-
-    Args:
-        a (int): The first number.
-        b (int): The second number.
-
-    Returns:
-	    int: The product of a and b.
-    """
-    return a * b
-
-
-print(multiply_numbers())
+numbers = {1, 2, 2, 3, 4, 1, 'm', 5, 'a'}
+numbers.
