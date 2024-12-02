d4b2a1a0e3b7f1fc46d7cc7bcb34a5e3b78f91b7
CosmicEmm <muhammadmalik5303@gmail.com>
2024-12-02 14:59:09 +0500

    Update scratch_pad.py

--- scratch_pad.py
+++ scratch_pad.py
@@ -1,7 +1,7 @@
-car_specs = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964,
-  "year": 2020
-}
-print(car_specs)
+numbers = [10, 5, 8, 20, 3, 15]
+maximum = numbers[0]
+for num in numbers[1:]:
+  if num > maximum:
+    maximum = num
+
+print(f'Largest Number: {maximum}')
