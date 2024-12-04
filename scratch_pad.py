ef72954816620e739a5c0f2a49ebf36886c8d88d
CosmicEmm <muhammadmalik5303@gmail.com>
2024-12-04 15:30:46 +0500

    Update scratch_pad.py

--- scratch_pad.py
+++ scratch_pad.py
@@ -1,2 +1,5 @@
-print("Hello " + "Friend")
-print([1, 2] + ['a', 'b'])
+numbers = [1, 2, 3, 4, 5]
+countries = {'GER':'Germany', 'FRA':'France', 'ENG':'England'}
+print(2 not in numbers)
+print(6 not in numbers)
+print('ENG' not in countries)
