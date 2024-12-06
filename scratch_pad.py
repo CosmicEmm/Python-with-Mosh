7143ce04fc1748800073d09167db8b611fdc1841
CosmicEmm <muhammadmalik5303@gmail.com>
2024-12-06 13:08:49 +0500

    Update scratch_pad.py

--- scratch_pad.py
+++ scratch_pad.py
@@ -1,8 +1,2 @@
-city = 'London'
-_year = '2024'
-print(city + _year)
-KAREN3212 = 'Manager'
-karen5303 = 'Employee'
-print(f'{karen5303} reports to {KAREN3212}.')
-is_online = True
-print(is_online)
+x = 5; y = x
+print(y)
