a8d8a764a1a78c71ae16ed0909852940e0ecb515
CosmicEmm <muhammadmalik5303@gmail.com>
2024-12-14 16:48:18 +0500

    Update scratch_pad.py

--- scratch_pad.py
+++ scratch_pad.py
@@ -1,2 +1,10 @@
-x = 5; y = x
-print(y)
+data = [1, 2, 3]
+
+def append_element(some_list, element):
+    some_list.append(element)
+    data = [5, 6, 7] # won't overwrite the variable 'data' outside the function
+    print(data)
+
+
+append_element(data, 4)
+print(data)
